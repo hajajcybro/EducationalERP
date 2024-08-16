8a6904f19777a1204f322c01f36b04eed359fb8a
Cybrosys Technologies <risvana@cybrosys.info>
2024-08-16 14:36:24 +0530

    Aug 16: [UPDT] Updated  Banner ' education_fee'

--- education_fee/__manifest__.py
+++ education_fee/__manifest__.py
@@ -45,7 +45,7 @@
     "demo": [
         # 'demo/fee_data.xml',
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     "installable": True,
     "auto_install": False,
